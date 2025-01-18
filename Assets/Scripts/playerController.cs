a4012bf1be1f72ec7732723b5c201ffc0244f438
austing320 <austingilbert320@gmail.com>
2025-01-18 05:18:17 -0500

    playerController Script

---
+++ Assets/Scripts/playerController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    public float speed = 5f;
+    public float jumpForce = 10f;
+    public Transform groundCheck;
+    public LayerMask groundLayer;
+    private Rigidbody2D rb;
+    private bool isGrounded;
+
+    void Start()
+    {
+        rb=GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+       isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.1f, groundLayer);
+       
+        float move = Input.GetAxis("Horizontal");
+        rb.velocity= new Vector2(move * speed, rb.velocity.y);
+
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+        }
+    }
+}
